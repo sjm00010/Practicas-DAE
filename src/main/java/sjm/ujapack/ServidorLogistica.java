5d9ffbc0a42dc1a00044cb63b5869b5b67c7e50b
Sergio Jiménez Moreno <sjm00010@red.ujaen.es>
2020-10-02 12:08:36 +0200

    Inicio del proyecto

---
+++ src/main/java/sjm/ujapack/ServidorLogistica.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sjm.ujapack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+
+/**
+ *
+ * @author sjm00010
+ */
+@EnableAutoConfiguration
+public class ServidorLogistica {
+    
+    public static void main(String[] args) throws Exception {
+        SpringApplication servidor = new SpringApplication(ServidorLogistica.class);
+        ApplicationContext context = servidor.run(args);
+    }
+}
