4e036a3cb740e6ffc47850c6ad2f9a019fbfb9b3
Sergio Jiménez Moreno <sjm00010@red.ujaen.es>
2020-10-04 12:02:14 +0200

    Cliente OK, Envio parcialmente OK(Falta PC y Paso)

---
+++ src/main/java/dae/clases/Cliente.java
@@ -0,0 +1,53 @@
+package dae.clases;
+
+/**
+ *
+ * @author sjm00010
+ */
+public class Cliente {
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private String localizacion;
+
+    public Cliente() {
+    }
+
+    public Cliente(String dni, String nombre, String apellidos, String localizacion) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.localizacion = localizacion;
+    }
+
+    /**
+     * @return the dni
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @return the apellidos
+     */
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    /**
+     * @return the localizacion
+     */
+    public String getLocalizacion() {
+        return localizacion;
+    }
+    
+    
+    
+}
---
+++ src/main/java/dae/clases/Envio.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dae.clases;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sjm00010
+ */
+public class Envio {
+    private int id;
+    private int alto;
+    private int ancho;
+    private int peso;
+    private Cliente origen;
+    private Cliente destino;
+    // FALTA el Array de Pasos, añadir tras su implementación
+    // private ArrayList<Paso> ruta;
+
+    public Envio() {
+    }
+
+    // Modificar tras añadir ruta
+    public Envio(int id, int alto, int ancho, int peso, Cliente origen, Cliente destino) {
+        this.id = id;
+        this.alto = alto;
+        this.ancho = ancho;
+        this.peso = peso;
+        this.origen = origen;
+        this.destino = destino;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the alto
+     */
+    public int getAlto() {
+        return alto;
+    }
+
+    /**
+     * @return the ancho
+     */
+    public int getAncho() {
+        return ancho;
+    }
+
+    /**
+     * @return the peso
+     */
+    public int getPeso() {
+        return peso;
+    }
+
+    /**
+     * @return the origen
+     */
+    public Cliente getOrigen() {
+        return origen;
+    }
+
+    /**
+     * @return the destino
+     */
+    public Cliente getDestino() {
+        return destino;
+    }
+    
+    // Añadir getRuta tras añadir ruta
+    
+    /**
+     * Función que establece la ruta para el envío
+     */
+    private void creaRuta(){
+        /* Es necesaria la implementacion de PuntoControl y Paso,
+           asi como la del ServicioMensajeria que es el que realiza
+           el calculo de la ruta */
+    }
+    
+    /**
+     * Función que calcula el precio de un envío
+     * @return precio
+     */
+    public int calculaPrecio(){
+        // Completar cuando se implementen la clase Paso
+        return peso*(alto*ancho); // * pasos.numPC / 100
+    }
+    
+    /**
+     * Función que permite actualizar el envío, modificando un Paso
+     */
+    public void actualizar(LocalDate fecha, boolean inOut){ // PuntoControl pc,
+        /* Se busca en los pasos aquel que tenga el PuntoControl igual al
+           dado y que coincida con el valor de inOut para añadirle la fecha */
+    }
+    
+}
