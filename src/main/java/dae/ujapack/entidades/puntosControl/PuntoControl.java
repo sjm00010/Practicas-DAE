f311cbcc2a14a27d1804057ae8026937f3b83556
Juan Carmona Cortés <jcc00078@red.ujaen.es>
2020-11-13 18:58:41 +0100

    Mapeo clase PuntoControl y las clases Oficina y CentroLogistico

--- src/main/java/dae/ujapack/entidades/puntosControl/PuntoControl.java
+++ src/main/java/dae/ujapack/entidades/puntosControl/PuntoControl.java
@@ -1,16 +1,27 @@
 package dae.ujapack.entidades.puntosControl;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.validation.constraints.NotBlank;
 
 /**
  * Clase que representa un punto de control
  * @author sjm00010
  */
-public abstract class PuntoControl {
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class PuntoControl implements Serializable {
     
     /* Id del punto de control */
+    @Id
     @NotBlank
     String id;
     
+    public PuntoControl() {}
+    public PuntoControl(String id) {this.id = id;}
     public String getId(){ return id; }
+    
 }
